fead166fc70de201ab7a5fa1d04e26a21b9a5cc5
gaiaprojectsol <gaiaprojectsol@gmail.com>
2025-11-20 20:42:11 +1030

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+export const CONFIG = {
+  BOT_USERNAME: "",
+  TELEGRAM_TOKEN: "",
+  OPENAI_API_KEY: "",
+  MODEL: "gpt-4.1-mini",
+
+  MEMORY_LIMIT: 30, // last N messages per user
+
+  // Solana RPC
+  SOLANA_RPC: "",
+};
